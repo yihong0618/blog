ebe16fd931f6a8a2062936d2a7ae027236caa6ed
yihong0618 <zouzou0208@gmail.com>
2020-07-13 19:00:56 +0800

    refactor: running.jsx

--- src/pages/running.jsx
+++ src/pages/running.jsx
@@ -1,11 +1,11 @@
 import React, { Fragment, useState } from "react";
-import { format as formatDate } from "date-fns";
 import { Helmet } from "react-helmet";
-import { graphql } from "gatsby";
+import * as mapboxPolyline from "@mapbox/polyline";
 import ReactMapGL, { Source, Layer } from "react-map-gl";
 import { ViewportProvider, useDimensions } from "react-viewport-utils";
 
 import Layout from "../components/layout";
+import { activities } from "../static/activities";
 
 import "mapbox-gl/dist/mapbox-gl.css";
 import styles from "./running.module.scss";
@@ -15,92 +15,118 @@ const MAPBOX_TOKEN =
 
 // Page
 
-export default ({ data }) => {
-  const runs = data.runs.nodes;
-  return (
-    <Fragment>
-      <Helmet bodyAttributes={{ class: styles.body }} />
-      <Layout>
-        <div className="mb5">
-          <div className="w-100">
-            <h1 className="f1 fw9 i">Running</h1>
-          </div>
-          <div className="fl w-100 w-30-l pb5 pr5-l">
-            <section className="pb4">
-              <p>
-                我用app记录自己跑步8年有余，下面列表展示的是今年的数据
-              </p>
-              <p>
-                现在我用NRC记录自己跑步{" "}
-                <a className="dark-gray b" href="https://www.nike.com/nrc-app">
-                  Nike Run Club
-                </a>{" "}
-              </p>
-            </section>
-            <section>
-              <Stat value={2020} description={"跑步旅程"} />
-              <Stat value={data.runs.totalCount} description={"Runs"} />
-              <Stat
-                value={calcMetricTotal(runs, "distance")}
-                description={"KM"}
-              />
-            </section>
-            <section className="mt4">
-              <Stat
-                value={`${calculateRunStreak(data.runs)} day`}
-                description={"streak"}
-                className="mb0 pb0"
-              />
-              <p className="mt0">
-                <small>Consecutive days run allowing for 1 rest day.</small>
-              </p>
-            </section>
-          </div>
-          <div className="fl w-100 w-70-l">
-            <RunMapWithViewport runs={runs} />
-            <RunTable runs={runs} />
-          </div>
+export default () => (
+  <Fragment>
+    <Helmet bodyAttributes={{ class: styles.body }} />
+    <Layout>
+      <div className="mb5">
+        <div className="w-100">
+          <h1 className="f1 fw9 i">Running</h1>
         </div>
-      </Layout>
-    </Fragment>
-  );
-};
-
-export const query = graphql`
-  query {
-    runs: allActivitiesJson(
-      sort: { fields: start_epoch_ms, order: DESC }
-      # Filter to above 60s to exclude cancelled runs
-      filter: { active_duration_ms: { gt: 60000 } }
-    ) {
-      nodes {
-        id
-        start_epoch_ms
-        summaries {
-          metric
-          value
-          summary
-        }
-        metrics {
-          type
-          values {
-            value
-          }
-        }
-      }
-      totalCount
-    }
-  }
-`;
+        <YearsStat runs={activities} />
+        <div className="fl w-100 w-70-l">
+          <RunMapWithViewport runs={activities} />
+          <RunTable runs={activities} year="2020" />
+        </div>
+      </div>
+    </Layout>
+  </Fragment>
+);
 
 // Child components
 
-const RunMap = ({ runs }) => {
+const YearsStat = ({ runs }) => {
+  // for short solution need to refactor
+  const yearsArr = [
+    "2012",
+    "2013",
+    "2014",
+    "2015",
+    "2016",
+    "2017",
+    "2018",
+    "2019",
+    "2020",
+  ].reverse();
+  return (
+    <div className="fl w-100 w-30-l pb5 pr5-l">
+      <section className="pb4">
+        <p>我用app记录自己跑步8年有余，下面列表展示的是今年的数据</p>
+        <p>
+          现在我用NRC记录自己跑步{" "}
+          <a className="dark-gray b" href="https://www.nike.com/nrc-app">
+            Nike Run Club
+          </a>{" "}
+        </p>
+      </section>
+      {yearsArr.map((year) => (
+        <YearStat runs={runs} year={year} />
+      ))}
+    </div>
+  );
+};
+
+const YearStat = ({ runs, year }) => {
+  runs = runs.filter((run) => run.start_date_local.slice(0, 4) === year);
+  let sumDistance = 0;
+  let streak = 0;
+  let pace = 0;
+  let paceNullCount = 0;
+  let heartRate = 0;
+  let heartRateNullCount = 0;
+  runs.forEach((run) => {
+    sumDistance += run.distance || 0;
+    if (run.average_speed) {
+      pace += run.average_speed;
+    } else {
+      paceNullCount++;
+    }
+    if (run.average_heartrate) {
+      heartRate += run.average_heartrate;
+    } else {
+      heartRateNullCount++;
+    }
+    if (run.streak) {
+      streak = Math.max(streak, run.streak);
+    }
+  });
+  sumDistance = (sumDistance / 1000.0).toFixed(1);
+  const avgPace = formatPace(pace / (runs.length - paceNullCount));
+  const hasHeartRate = !(heartRate === 0);
+  const avgHeartRate = (heartRate / (runs.length - heartRateNullCount)).toFixed(
+    0
+  );
+  return (
+    <div>
+      <section>
+        <Stat value={year} description="跑步旅程" />
+        <Stat value={runs.length} description="Runs" />
+        <Stat value={sumDistance} description="KM" />
+        <Stat value={avgPace} description="Avg Pace" />
+        <Stat
+          value={`${streak} day`}
+          description="streak"
+          className="mb0 pb0"
+        />
+        {hasHeartRate && (
+          <Stat value={avgHeartRate} description="Avg Heart Rate" />
+        )}
+      </section>
+      <hr />
+    </div>
+  );
+};
+
+const RunMap = ({ runs, year }) => {
+  year = year || "2020";
+  const isSingleRun = runs.length === 1;
+  const geoData = geoJsonForRuns(runs, year);
+  const startPoint = geoData.features[0][0];
   const [viewport, setViewport] = useState({
     width: "100%",
     height: 400,
-    latitude: 38.862,
-    longitude: 121.514,
+    latitude: isSingleRun ? startPoint[0] : 38.862,
+    longitude: isSingleRun ? startPoint[1] : 121.514,
     zoom: 11.5,
   });
 
@@ -125,7 +151,7 @@ const RunMap = ({ runs }) => {
       onViewportChange={setViewport}
       mapboxApiAccessToken={MAPBOX_TOKEN}
     >
-      <Source id="data" type="geojson" data={geoJsonForRuns(runs)}>
+      <Source id="data" type="geojson" data={geoData}>
         <Layer
           id="runs"
           type="line"
@@ -149,83 +175,84 @@ const RunMapWithViewport = (props) => (
   </ViewportProvider>
 );
 
-const RunTable = ({ runs }) => (
-  <div className={styles.tableContainer}>
-    <table className={styles.runTable} cellSpacing="0" cellPadding="0">
-      <thead>
-        <tr>
-          <th></th>
-          <th>KM</th>
-          <th>Pace</th>
-          <th>BPM</th>
-          <th></th>
-        </tr>
-      </thead>
-      <tbody>
-        {runs.map((run) => (
-          <RunRow run={run} key={run.id} />
-        ))}
-      </tbody>
-    </table>
-  </div>
-);
+const RunTable = ({ runs, year }) => {
+  runs = runs.filter((run) => run.start_date_local.slice(0, 4) === year);
+  runs = runs.sort((a, b) => {
+    return new Date(b.start_date_local) - new Date(a.start_date_local);
+  });
+  return (
+    <div className={styles.tableContainer}>
+      <table className={styles.runTable} cellSpacing="0" cellPadding="0">
+        <thead>
+          <tr>
+            <th />
+            <th>KM</th>
+            <th>Pace</th>
+            <th>BPM</th>
+            <th />
+          </tr>
+        </thead>
+        <tbody>
+          {runs.map((run) => (
+            <RunRow run={run} key={run.strava_id} />
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
 
 const RunRow = ({ run }) => {
-  const distance = run.summaries.find(
-    (x) => x.summary === "total" && x.metric === "distance"
-  );
+  const distance = (run.distance / 1000.0).toFixed(1);
+  const pace = run.average_speed;
 
-  const pace = run.summaries.find(
-    (x) => x.summary === "mean" && x.metric === "pace"
-  );
+  const paceParts = pace ? formatPace(pace) : null;
 
-  const paceParts = pace ? decimalToMinsSecs(pace.value) : null;
-
-  const heartRate = run.summaries.find(
-    (x) => x.summary === "mean" && x.metric === "heart_rate"
-  );
+  const heartRate = run.average_heartrate;
 
   return (
     <tr className={styles.runRow}>
       <td>{titleForRun(run)}</td>
-      <td>{distance && distance.value.toFixed(2)}</td>
-      {pace && (
-        <td>
-          {paceParts.minutes}:
-          {paceParts.seconds < 10 ? `0${paceParts.seconds}` : paceParts.seconds}{" "}
-        </td>
-      )}
-      <td>{heartRate && heartRate.value.toFixed(0)}</td>
-      <td className={styles.runDate}>
-        {formatDate(new Date(run.start_epoch_ms), "do MMM yyyy")}
-      </td>
+      <td>{distance}</td>
+      {pace && <td>{paceParts}</td>}
+      <td>{heartRate && heartRate.toFixed(0)}</td>
+      <td className={styles.runDate}>{run.start_date_local}</td>
     </tr>
   );
 };
 
-const Stat = ({ value, description, className }) => (
-  <div className={`${className} pb2 w-100`}>
-    <span className="f1 fw9 i">{intComma(value)} </span>
-    <span className="f3 fw6 i">{description}</span>
-  </div>
-);
+const Stat = ({ value, description, className }) => {
+  const isYear = value === "2020";
+  return (
+    <div className={`${className} pb2 w-100`}>
+      <span className="f1 fw9 i">{intComma(value)} </span>
+      {/* {isYear && <button onClick={handleYearClick}>Button Test</button>} */}
+      <span className="f3 fw6 i">{description}</span>
+    </div>
+  );
+};
 
 // Utilities
 
-const calcMetricTotal = (runs, metric) =>
-  runs
-    .reduce((distance, run) => {
-      const summary = run.summaries.find((s) => s.metric === metric);
-      return summary ? distance + summary.value : distance;
-    }, 0)
-    .toFixed(0);
-
 const intComma = (x) => x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 
-const geoJsonForRuns = (runs) => ({
-  type: "FeatureCollection",
-  features: runs
-    .map((run) => {
+const pathForRun = (run) => {
+  try {
+    const c = mapboxPolyline.decode(run.summary_polyline);
+    c.forEach((arr) => {
+      [arr[0], arr[1]] = [arr[1], arr[0]];
+    });
+    return c;
+  } catch (err) {
+    return [];
+  }
+};
+
+const geoJsonForRuns = (runs, year) => {
+  runs = runs.filter((run) => run.start_date_local.slice(0, 4) === year);
+  return {
+    type: "FeatureCollection",
+    features: runs.map((run) => {
       const points = pathForRun(run);
       if (!points) {
         return null;
@@ -238,81 +265,27 @@ const geoJsonForRuns = (runs) => ({
           coordinates: points,
         },
       };
-    })
-    .filter((x) => !!x),
-});
-
-const pathForRun = (run) => {
-  try {
-    const lats = run.metrics
-      .find((x) => x.type === "latitude")
-      .values.map((x) => x.value);
-    const longs = run.metrics
-      .find((x) => x.type === "longitude")
-      .values.map((x) => x.value);
-    return longs.map((x, idx) => [x, lats[idx]]);
-  } catch (err) {
-    return [];
-  }
+    }),
+  };
 };
 
 const titleForRun = (run) => {
+  if (run.name.slice(0, 7) === "Running") {
+    return "Run";
+  }
+  if (run.name) {
+    return run.name;
+  }
   return "Run";
 };
 
-const capWord = (word) => {
-  word = word.toLowerCase();
-
-  const ignores = ["a", "the", "of", "to", "with"];
-
-  if (ignores.includes(word)) {
-    return word;
-  }
-  return word.charAt(0).toUpperCase() + word.substr(1);
+const formatPace = (d) => {
+  const pace = (1000.0 / 60.0) * (1.0 / d);
+  const minutes = Math.floor(pace);
+  const seconds = (pace - minutes) * 60.0;
+  return `${minutes}:${seconds.toFixed(0).toString().padStart(2, "0")}`;
 };
 
-const decimalToMinsSecs = (value) => {
-  const minutes = Math.floor(value);
-  const rem = value - minutes;
-  const seconds = (60 * rem).toFixed(0);
-  return { minutes, seconds };
-};
-
-const calculateRunStreak = (runs) => {
-  let count = 1;
-  let hasHadRestDay = false;
-  let skippingRestDay = false;
-  let lastDay = roundDate(new Date(runs.nodes[0].start_epoch_ms));
-
-  const oneDay = 86400000;
-  const twoDays = 86400000 * 2;
-
-  for (let i = 1; i < runs.nodes.length; i++) {
-    const day = roundDate(new Date(runs.nodes[i].start_epoch_ms));
-    const diff = lastDay - day;
-    if (diff === 0) {
-      continue;
-    }
-    if (diff === oneDay || skippingRestDay) {
-      lastDay = day;
-      count += 1;
-      skippingRestDay = false;
-    } else if (!hasHadRestDay && diff === twoDays) {
-      hasHadRestDay = true;
-      skippingRestDay = true;
-      lastDay = day;
-      count += 1;
-    } else {
-      return count;
-    }
-  }
-  return count;
-};
-
-const roundDate = (date) => {
-  date.setHours(0);
-  date.setMinutes(0);
-  date.setSeconds(0);
-  date.setMilliseconds(0);
-  return date;
+const handleYearClick = () => {
+  console.log(11111111111);
 };
