43cd2f9ce8d198720729e18e240b00af481649a7
yihong0618 <zouzou0208@gmail.com>
2020-07-17 13:50:08 +0800

    change: tiny logger

--- scripts/strava.py
+++ scripts/strava.py
@@ -197,7 +197,7 @@ def make_new_gpxs(files):
     gpx_files = os.listdir(GPX_FOLDER)
     for f in gpx_files:
         upload_gpx(GPX_FOLDER + "/" + f)
-        print(f +" uploaded")
+        logger.info(f +" uploaded")
 
 
 if __name__ == "__main__":
