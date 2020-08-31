3ebcb53cbc3d96b3982ebd912f4e496899cf6a67
yihong0618 <zouzou0208@gmail.com>
2020-08-31 23:26:21 +0800

    fix: gpx bug

--- scripts/nike_sync.py
+++ scripts/nike_sync.py
@@ -28,6 +28,7 @@ class Nike:
                 "client_id": NIKE_CLIENT_ID,
                 "grant_type": "refresh_token",
             },
+            timeout=60
         )
         response.raise_for_status()
 
