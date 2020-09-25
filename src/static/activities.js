a2738c2818392906d6b06e35c98ac81806f5c55b
yihong0618 <zouzou0208@gmail.com>
2020-09-25 14:17:00 +0000

    Add new runs

--- src/static/activities.js
+++ src/static/activities.js
@@ -28053,6 +28053,23 @@ const activities = [
     "average_heartrate": 147.4,
     "average_speed": 2.933,
     "streak": 133
+  },
+  {
+    "strava_id": 4110312353,
+    "athlete_id": 58330365,
+    "name": "\u665a\u95f4\u8dd1\u6b65",
+    "distance": 2781.0,
+    "moving_time": "0:15:17",
+    "elapsed_time": "0:15:17",
+    "total_elevation_gain": 75.4,
+    "type": "Run",
+    "start_date": "2020-09-25 13:08:48+00:00",
+    "start_date_local": "2020-09-25 21:08:48",
+    "location_country": "\u5e7f\u8d24\u8def, \u51cc\u6c34\u8857\u9053, \u7518\u4e95\u5b50\u533a, \u5927\u8fde\u5e02, \u8fbd\u5b81\u7701, 116085, China \u4e2d\u56fd",
+    "summary_polyline": "_ielFmctdVKQODGX?TIr@GrABZPJR?VGJQA[FYEU@[B[F[CWMQSIORCZDXe@bB@p@RJh@EHSAWBYGs@HWC[F[KUSEONCT?ZKr@?VCVKRAZFVV@RELSDSDu@E[NuAQSQIOROn@Gn@?p@MT?XBZV?RCNOFw@Aq@Hs@@]GUQGQJSbDKT@XLNh@EHYCWR@IY?[LmAIYOOS?KlAIXEp@EZHVJPR?RMBaDDWEYSKOJGREZAl@Kr@Ct@@Vh@@Ti@Aq@GUBWJWDUE]OQSAKNKX?ZIVB^MV?ZGVDTRFRARIDW@WFYAYB[DYJSEWMMSGQLGXGl@Qp@CnARNTCTBJQF]BYMw@Lm@@[QOQIONCVWn@GjAMTBVLRPDTGRKHU?[Cw@Hy@?[OOSGKPMr@Ur@@ZCp@\\\\R@TCZc@D[C[IU?[DW",
+    "average_heartrate": 155.6,
+    "average_speed": 3.033,
+    "streak": 134
   }
 ];
 
