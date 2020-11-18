6ba5bb0987ae3506a805d0588eeb1d7db3c5748b
yihong0618 <zouzou0208@gmail.com>
2020-11-18 13:13:30 +0000

    Add new runs

--- src/static/activities.js
+++ src/static/activities.js
@@ -28920,6 +28920,23 @@ const activities = [
     "average_heartrate": 144.4,
     "average_speed": 3.079,
     "streak": 3
+  },
+  {
+    "strava_id": 4355371379,
+    "athlete_id": 58330365,
+    "name": "\u508d\u665a\u8dd1\u6b65",
+    "distance": 1040.6,
+    "moving_time": "0:06:03",
+    "elapsed_time": "0:06:23",
+    "total_elevation_gain": 9.7,
+    "type": "Run",
+    "start_date": "2020-11-18 12:27:58+00:00",
+    "start_date_local": "2020-11-18 20:27:58",
+    "location_country": "\u5e7f\u8d24\u8def, \u51cc\u6c34\u8857\u9053, \u5927\u8fde\u5e02, \u8fbd\u5b81\u7701, 116085, China \u4e2d\u56fd",
+    "summary_polyline": "yielFeitdVx@b@NTNLzAbAZ^dAp@RFLNLVPFLNR?LONk@BYRo@VeBHSDo@Tq@Ju@HQTiARk@LQFU?[LgBDWVm@?YQCi@Ug@Ke@YMOSIkAu@c@USCSBg@Jc@VUDKPKTEZSn@?XIVMTIT[f@GVKP",
+    "average_heartrate": 144.9,
+    "average_speed": 2.867,
+    "streak": 1
   }
 ];
 
