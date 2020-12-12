655e7a75007c288d61099e78c9b83bb252576c56
yihong0618 <zouzou0208@gmail.com>
2020-12-12 11:17:34 +0000

    Add new runs

--- src/static/activities.js
+++ src/static/activities.js
@@ -29260,6 +29260,23 @@ const activities = [
     "average_heartrate": 147.8,
     "average_speed": 2.921,
     "streak": 9
+  },
+  {
+    "strava_id": 4461568039,
+    "athlete_id": 58330365,
+    "name": "\u508d\u665a\u8dd1\u6b65",
+    "distance": 2587.8,
+    "moving_time": "0:14:57",
+    "elapsed_time": "0:14:57",
+    "total_elevation_gain": 10.7,
+    "type": "Run",
+    "start_date": "2020-12-12 10:20:56+00:00",
+    "start_date_local": "2020-12-12 18:20:56",
+    "location_country": "\u5e7f\u8d24\u8def, \u7518\u4e95\u5b50\u533a, \u51cc\u6c34\u8857\u9053, \u7518\u4e95\u5b50\u533a, \u5927\u8fde\u5e02, \u8fbd\u5b81\u7701, 116085, China \u4e2d\u56fd",
+    "summary_polyline": "cielF{htdVNRPNjAx@t@`@LNr@f@r@r@bAh@PRTJvAfANP`@VTFj@?`C?|ABRCf@@PBRCfB@JUD[@mAAY?{@Ey@@[Eq@?[Eq@Bs@Aw@EUBYHSB_D]c@g@Ww@]oBq@iAq@QISCQIB]HUFWNmANq@LS?WCUDW^eABYCkARo@Ly@l@u@I_@JkACWB[J[Zk@DWRq@?WToADs@RcAG[MUq@{@a@WQGQSa@WQQi@UgAy@a@a@QGa@]QKc@]m@o@SK_@[a@Q[c@g@[k@OQI_@c@UGsAcAQI?WNQVi@TKZc@t@e@f@CJS`@WLOHSROFSRGTBZa@?Y",
+    "average_heartrate": 157.5,
+    "average_speed": 2.885,
+    "streak": 1
   }
 ];
 
