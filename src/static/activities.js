9afafe38560fa89f91ee1cbfed9c0c9ff0bbafbc
yihong0618 <zouzou0208@gmail.com>
2020-12-14 14:20:21 +0000

    Add new runs

--- src/static/activities.js
+++ src/static/activities.js
@@ -29294,6 +29294,23 @@ const activities = [
     "average_heartrate": 146.9,
     "average_speed": 3.145,
     "streak": 2
+  },
+  {
+    "strava_id": 4471304801,
+    "athlete_id": 58330365,
+    "name": "\u665a\u95f4\u8dd1\u6b65",
+    "distance": 1026.1,
+    "moving_time": "0:05:32",
+    "elapsed_time": "0:05:32",
+    "total_elevation_gain": 8.7,
+    "type": "Run",
+    "start_date": "2020-12-14 13:39:36+00:00",
+    "start_date_local": "2020-12-14 21:39:36",
+    "location_country": "\u5e7f\u8d24\u8def, \u7518\u4e95\u5b50\u533a, \u51cc\u6c34\u8857\u9053, \u7518\u4e95\u5b50\u533a, \u5927\u8fde\u5e02, \u8fbd\u5b81\u7701, 116085, \u4e2d\u56fd",
+    "summary_polyline": "wielFgitdVPLZ`@j@TPN`@VLRRDJRPFhAv@RH^d@PNRLR@DUAs@Pq@d@qALq@J]DUPk@XiBRq@BW^}AJs@@[FYJW@q@ISQIc@KQOg@QkAq@i@QS?UGKU_AQu@^OLKPAZEVSr@MRW`AKRGX",
+    "average_heartrate": 149.2,
+    "average_speed": 3.091,
+    "streak": 3
   }
 ];
 
