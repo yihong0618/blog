280b8bb52459db1131b507a793a451c0f0e47b26
yihong0618 <zouzou0208@gmail.com>
2021-02-14 14:20:30 +0000

    Add new runs

--- src/static/activities.js
+++ src/static/activities.js
@@ -30246,6 +30246,23 @@ const activities = [
     "average_heartrate": 160.7,
     "average_speed": 2.868,
     "streak": 45
+  },
+  {
+    "strava_id": 4786329099,
+    "athlete_id": 58330365,
+    "name": "\u665a\u95f4\u8dd1\u6b65",
+    "distance": 546.0,
+    "moving_time": "0:03:13",
+    "elapsed_time": "0:03:13",
+    "total_elevation_gain": 12.8,
+    "type": "Run",
+    "start_date": "2021-02-14 13:54:07+00:00",
+    "start_date_local": "2021-02-14 21:54:07",
+    "location_country": "\u5e7f\u8d24\u8def, \u7518\u4e95\u5b50\u533a, \u51cc\u6c34\u8857\u9053, \u7518\u4e95\u5b50\u533a, \u5927\u8fde\u5e02, \u8fbd\u5b81\u7701, 116085, \u4e2d\u56fd",
+    "summary_polyline": "{gelFectdVKQSKa@fAM`B?XHTRHR@LMFu@AkALm@MYSQOHUl@GVEp@@\\At@JRd@ENO@WDiBBWHSKWSMSDKREX",
+    "average_heartrate": null,
+    "average_speed": 2.829,
+    "streak": 46
   }
 ];
 
