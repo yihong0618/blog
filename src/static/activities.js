76b1105468f870c358421f47b23660fbd6fd17f7
yihong0618 <zouzou0208@gmail.com>
2021-07-13 14:17:28 +0000

    Add new runs

--- src/static/activities.js
+++ src/static/activities.js
@@ -32847,6 +32847,23 @@ const activities = [
     "average_heartrate": 138.7,
     "average_speed": 2.788,
     "streak": 2
+  },
+  {
+    "strava_id": 5620889464,
+    "athlete_id": 58330365,
+    "name": "\u665a\u95f4\u8dd1\u6b65",
+    "distance": 2718.3,
+    "moving_time": "0:15:15",
+    "elapsed_time": "0:15:15",
+    "total_elevation_gain": 71.7,
+    "type": "Run",
+    "start_date": "2021-07-13 13:12:06+00:00",
+    "start_date_local": "2021-07-13 21:12:06",
+    "location_country": "\u5e7f\u8d24\u8def, \u7518\u4e95\u5b50\u533a, \u51cc\u6c34\u8857\u9053, \u7518\u4e95\u5b50\u533a, \u5927\u8fde\u5e02, \u8fbd\u5b81\u7701, 116085, \u4e2d\u56fd",
+    "summary_polyline": "qhelFcctdVNOMOQ?EXSV?XSh@Bv@KZ?VRHTBTALOFY@[Gy@AWJu@KWSISHIt@KRGVBr@Ap@JRf@DPMF[BWMu@?WLu@GUQGSCGTMp@Sj@AhBTHPBTANK@YA[BWEo@F[Fu@UKSEMPMp@?XIVCn@Gv@NLTBRANOFq@HWI]CW?WF[@WQQSAMPGXShAAVBXEr@PNTDVANSD[Bo@Iu@JYEUBUQMUEWd@It@MTCZFZCXGTPTTDT@PQF{@EYMU?]Rs@C[SIQCMTCZMr@IRBZClANLPDX@NMFU@]AYIYC]D[JUC[MKSAOLMp@?XMl@@nARNP@TEJSCiBCWJYBWEYQGSFSn@AXKTAV@XCt@DTh@DPG@YA[DUEYCYBYFW@WKQQGON_@|AFx@Cp@RLRDRCLWGY@s@GU@YJSBYAWSKSDMPGp@IT",
+    "average_heartrate": 151.1,
+    "average_speed": 2.971,
+    "streak": 3
   }
 ];
 
