9551ad814b754416b1bb4af6f94f4d727e212350
yihong0618 <zouzou0208@gmail.com>
2021-11-30 12:24:59 +0000

    Add new runs

--- src/static/activities.js
+++ src/static/activities.js
@@ -34802,6 +34802,23 @@ const activities = [
     "average_heartrate": 156.0,
     "average_speed": 2.701,
     "streak": 3
+  },
+  {
+    "strava_id": 6325296732,
+    "athlete_id": 58330365,
+    "name": "\u508d\u665a\u8dd1\u6b65",
+    "distance": 1641.2,
+    "moving_time": "0:09:36",
+    "elapsed_time": "0:10:55",
+    "total_elevation_gain": 12.9,
+    "type": "Run",
+    "start_date": "2021-11-30 11:12:32+00:00",
+    "start_date_local": "2021-11-30 19:12:32",
+    "location_country": "\u9ad8\u80fd\u8857, \u7518\u4e95\u5b50\u533a, \u51cc\u6c34\u8857\u9053, \u7518\u4e95\u5b50\u533a, \u5927\u8fde\u5e02, \u8fbd\u5b81\u7701, 116026, \u4e2d\u56fd",
+    "summary_polyline": "aoelFgetdVD?jAaBNQPATJLRh@Zb@`@rBrA`@XNPt@b@`@Zh@Tn@r@vAbA^^lADpABTCRBtBAR@PArADnAC?YBq@As@Hs@EmADw@CY@sAE]FmAEW@w@DY?WC[DU?]GW?YMQc@YSISCOKUGUEOOi@MSGSQ_Bu@S?c@JqCmAu@c@kAc@OKi@KOKk@EOPg@NOLCV",
+    "average_heartrate": 150.0,
+    "average_speed": 2.849,
+    "streak": 1
   }
 ];
 
