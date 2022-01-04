1385164bfff3e5e4b13172aa4b8b44213cfad710
yihong0618 <zouzou0208@gmail.com>
2022-01-04 13:27:06 +0000

    Add new runs

--- src/static/activities.js
+++ src/static/activities.js
@@ -35278,6 +35278,23 @@ const activities = [
     "average_heartrate": 163.1,
     "average_speed": 2.856,
     "streak": 6
+  },
+  {
+    "strava_id": 6471129143,
+    "athlete_id": 58330365,
+    "name": "\u508d\u665a\u8dd1\u6b65",
+    "distance": 3184.3,
+    "moving_time": "0:18:47",
+    "elapsed_time": "0:19:50",
+    "total_elevation_gain": 29.5,
+    "type": "Run",
+    "start_date": "2022-01-04 12:41:23+00:00",
+    "start_date_local": "2022-01-04 20:41:23",
+    "location_country": "\u9ad8\u80fd\u8857, \u7518\u4e95\u5b50\u533a, \u51cc\u6c34\u8857\u9053, \u7518\u4e95\u5b50\u533a, \u5927\u8fde\u5e02, \u8fbd\u5b81\u7701, 116026, \u4e2d\u56fd",
+    "summary_polyline": "coelF{dtdVv@oA^_@RITFRNh@X`@\\r@d@NPb@RPPRHb@\\RJPNRJLNRJPNRFLLHs@HSFs@J[Ns@Ju@FWJSTqAJSLo@@YTeAHqANW@]VqARq@Qk@OGiAq@KQc@[SKQAs@o@QAg@Qk@BMRUHw@YWm@MOe@S[a@i@MQHSCQQSEgAq@oA_@SCMNEZ?XU~A[hACXGXAVSl@MlAQn@e@|BIt@Kn@CVNHTBb@Rx@JfClAh@JPHx@RPJfCnBdAl@vAhAPHn@j@hAv@r@l@b@Vt@h@RDT@x@@h@E|@?x@BPCh@?T?RDh@@nACLKDY?_CCgBGy@@[Ew@JgBCUDoAEmA?YBWAWgAk@c@MSKSCQKUGSMoAg@y@QSAQBSCeAq@gAi@",
+    "average_heartrate": 156.4,
+    "average_speed": 2.825,
+    "streak": 7
   }
 ];
 
