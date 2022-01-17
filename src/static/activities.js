3bee47e2314c6bc1b283cbab0c075d86033c87d4
yihong0618 <zouzou0208@gmail.com>
2022-01-17 22:19:51 +0000

    Add new runs

--- src/static/activities.js
+++ src/static/activities.js
@@ -35516,6 +35516,23 @@ const activities = [
     "average_heartrate": 164.5,
     "average_speed": 2.98,
     "streak": 20
+  },
+  {
+    "strava_id": 6540295682,
+    "athlete_id": 58330365,
+    "name": "\u6668\u95f4\u8dd1\u6b65",
+    "distance": 1516.9,
+    "moving_time": "0:08:37",
+    "elapsed_time": "0:08:50",
+    "total_elevation_gain": 39.9,
+    "type": "Run",
+    "start_date": "2022-01-17 21:49:14+00:00",
+    "start_date_local": "2022-01-18 05:49:14",
+    "location_country": "\u5e7f\u8d24\u8def, \u7518\u4e95\u5b50\u533a, \u51cc\u6c34\u8857\u9053, \u7518\u4e95\u5b50\u533a, \u5927\u8fde\u5e02, \u8fbd\u5b81\u7701, 116085, \u4e2d\u56fd",
+    "summary_polyline": "gjelF_dtdVJQKPGV?XMr@FZDx@HVRHR@RIEW?[FmAHu@MUOKWBIRKn@GTAZDXCr@HRd@FPEDWEw@CWB[Ru@EWSIS?GTKRSrADr@CVHVf@BHUA[BYC[NiALSMWOIUBMNEn@IVGXEp@?XFVRJTBPGJS@YCY@YEWHsAMSSCQNQhAMRHn@Ev@PPh@@FYC[BYCs@HWDw@OQSGQJGXGt@IXFXKr@?XPLf@FLUA[BWDYI[ViA",
+    "average_heartrate": 168.3,
+    "average_speed": 2.934,
+    "streak": 21
   }
 ];
 
