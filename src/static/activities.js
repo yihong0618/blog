d7feaae92640dd246c40515329760193bb836bce
yihong0618 <zouzou0208@gmail.com>
2022-01-23 12:28:53 +0000

    Add new runs

--- src/static/activities.js
+++ src/static/activities.js
@@ -35618,6 +35618,23 @@ const activities = [
     "average_heartrate": null,
     "average_speed": 2.933,
     "streak": 3
+  },
+  {
+    "strava_id": 6568575951,
+    "athlete_id": 58330365,
+    "name": "\u508d\u665a\u8dd1\u6b65",
+    "distance": 8161.9,
+    "moving_time": "0:46:40",
+    "elapsed_time": "0:48:15",
+    "total_elevation_gain": 66.6,
+    "type": "Run",
+    "start_date": "2022-01-23 10:49:35+00:00",
+    "start_date_local": "2022-01-23 18:49:35",
+    "location_country": "\u9ad8\u80fd\u8857, \u7518\u4e95\u5b50\u533a, \u51cc\u6c34\u8857\u9053, \u7518\u4e95\u5b50\u533a, \u5927\u8fde\u5e02, \u8fbd\u5b81\u7701, 116026, \u4e2d\u56fd",
+    "summary_polyline": "ioelFketdVHUROJQHW`@_@RITH`@^THb@b@`@ZlAp@\\ZJPNHb@^TFf@V^\\NHd@\\LPf@VtAbAz@Fx@Cj@@PCh@BTC~@Fh@?f@A|@@h@CRFAVBV?|@Ip@Fz@Et@HTDv@@hBCZ@t@EX@pEBVEzC@vC@XLn@@XE|B@r@AhA?jBCXBVLVTHRIRBf@GzBQzBYtAMbBc@~Au@VGvCqATETKzBc@pAM~BAtANPCf@FRAf@NxDxAx@l@vBhALLRLf@VfAr@nAj@f@^t@d@^\\xB|@RJdBl@REp@w@h@WTIj@I|@CRKbCkEZ_@Zk@x@oBLOXg@To@j@uCHu@Au@[gCISWiAUk@i@wAKOQKIUg@}@[gAc@aAOU_@eAo@wAm@}@OOSGMMe@y@QSo@k@e@]g@Ue@[WCe@Ua@WU?US{CuAkAq@SIUCc@S}Ak@yAy@]c@QIOQEUISQOk@OmAg@s@c@OOu@_@s@m@SIq@e@SAy@[OQ_@[MUcA}@MQ_AkDIu@SoAi@}Ai@uBc@aAYe@GUq@mAQIQEOOISSGy@g@a@_@UGMSc@Sy@q@OISCOMSUMUa@]}A{@{BaBy@c@uAmAyAgA_@]u@a@oAqAg@Uu@o@e@Se@[o@u@SOSIa@a@QIy@c@]_@a@Yg@]e@SOK?YPq@AUOQfAgArB{@lAu@PAf@w@RkACWKWQMcAcBc@_@We@K[c@[MQOKMQc@[[a@U?QN]f@MTEXc@~@GXMLIXsB~DGTBXNNT?PHNT`@`@vA`A^`@PDPGLOf@Wf@Q`@[VBNJd@[f@QRCRGJOh@}AAWIWSQOIQUOm@Wm@q@o@Y_@e@YMSOMKSc@SKSOQQCSLMNe@|@Sh@IXwBrEk@xADVb@RrAbANPd@P",
+    "average_heartrate": 156.9,
+    "average_speed": 2.915,
+    "streak": 4
   }
 ];
 
